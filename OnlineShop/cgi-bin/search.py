61c0d715a45302e73094dbc4683dfc1f591ddc69
Anmol Arora <anmolarora1711@gmail.com>
2020-01-10 12:37:10 +0530

    Code

---
+++ OnlineShop/cgi-bin/search.py
@@ -0,0 +1,20 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+print("Content-type: text/html\r\n\r\n")
+
+import DB, base, cgi
+
+fieldStorage = cgi.FieldStorage()
+query = fieldStorage.getvalue('q')
+query = query.lower()
+
+for category in DB.searchIntents.keys():
+    if query in DB.searchIntents[category]:
+        query = category
+
+base.header()
+
+# print(query)
+
+for product in DB.products:
+    if query in product['product_category'] or query in product['product_brand'].lower() or query in product['product_name'].lower():
+        base.createProduct(product)
